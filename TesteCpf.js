f0a600b40890f2330df889eaff79cbf6b39d5c60
Lara Leão <laraleao@hotmail.com>
2020-08-26 17:03:02 -0300

    adição de html, comentários e teste realizado

--- TesteCpf.js
+++ TesteCpf.js
@@ -15,8 +15,10 @@
 // pegar resto
 
 function testeCpf() {
-  const cpf = prompt("Digite seu cpf:");
+  let cpf = document.getElementById("insere-cpf").value;
+  // para pegar o que foi digitado - apenas o seu valor
   const numero = cpf.length;
+  // para percorrer os números
 
   // verificação para saber se há 11 números
   if (numero !== 11) {
@@ -24,26 +26,46 @@ function testeCpf() {
   } else {
     alert("Cpf válido");
   }
+
+  let soma = 0;
+  let resto = 0;
+  let valido = false;
+    // 0 - 9 pois quero da posição 0 à 8 (9 dígitos);
+  for (i = 0; i < 9; i++) {
+    // substring usado para pegar as posições 1 à 1
+    soma = soma + cpf.substring((i - 1, i) * (11 - i));
+    // i - 1 será da posição 0 ao 1, que será o i
+    // 11 - i, são as posições sendo multplicadas
+    // resultado da soma, multiplicado por 10 e dividido por 11
+  }
+  resto = (soma * 10) % 11;
+
+  if (resto == 10 || resto == 11) resto = 0;
+  // se resto for diferente do primeiro validador (9), não entro nem no segundo e já retorno falso
+  var abc = parseInt(cpf.substring(9, 10););
+  if (resto != abc) {
+    valido = false;
+  }
+
+  // 1 - 10 pois quero da posição 1 à 9 (9 dígitos);
+  let soma2 = 0;
+  // visto que já foi usado uma soma acima
+  for (i = 1; i <= 10; i++) {
+    soma2 = soma2 + cpf.substring((i - 1, i) * (12 - i));
+    resto = (soma2 * 10) % 11;
+    // resultado da soma, multiplicado por 10 e dividido por 11
+  }
+
+  if (resto == 10 || resto == 11) resto = 0;
+  if (resto != parseInt(cpf.substring(10, 11))) valido = false;
+  valido = true;
+
+  if (valido == true) {
+    alert("Cpf válido");
+  } else {
+    alert("Cpf inválido");
+  }
 }
-let soma = 0;
-let resto = 0;
-
-// 0 - 9 pois quero da posição 0 à 8 (9 dígitos);
-for (i = 0; i < 9; i++) soma = soma + cpf.substring((i - 1, i) * (11 - i));
-resto = (soma * 10) % 11;
-
-if (resto == 10 || resto == 11) resto = 0;
-if (resto != cpf.substring(9, 10)) return false;
-
-// 0 - 9 pois quero da posição 0 à 8 (9 dígitos);
-soma = 0;
-for (i = 1; i <= 10; i++) soma = soma + cpf.substring((i - 1, i) * (12 - i));
-resto = (soma * 10) % 11;
-
-if (resto == 10 || resto == 11) resto = 0;
-if (resto != cpf.substring(10, 11)) return false;
-return true;
-
 // funcionamento substring - aula
 
 // var nome = "Lara";
